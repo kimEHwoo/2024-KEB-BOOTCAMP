7b9f08df90825f023b47cc994474fe3be88d97b7
김태우 <kimtaewoo721@gmail.com>
2024-01-15 14:14:41 +0900

    v0.9

--- day02.py
+++ day02.py
@@ -1,11 +1,9 @@
-first_number = int(input("First number : "))
-second_number = int(input("Second number :"))
-
-print(f'몫은 {divmod(first_number, second_number)[0]} 나머지는 {divmod(first_number, second_number)[1]}입니다')
-
-# first_number = int(input("First number : "))
-# second_number = int(input("Second number :"))
-#
-# quotient = first_number//second_number
-# remainder = first_number%second_number
-# print(f'몫은 {quotient} 나머지는 {remainder}입니다')
+dec = 65
+octal = 0o101
+hexadecimal = 0x41
+binary = 0b01000001
+print(dec,octal,hexadecimal,binary)
+print(chr(dec), chr(octal), chr(hexadecimal), chr(binary))
+#chr은 ASCII코드로 보여주는 함수이다. 결과는 A
+print(bin(dec), bin(octal), bin(hexadecimal), bin(binary))
+print(ord('B'), ord('Z'),ord('a'), ord('2')) #66,90,97,50
