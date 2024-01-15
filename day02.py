7e29de1f40d4f1167be8ba8d02f4e376bbd691a8
김태우 <kimtaewoo721@gmail.com>
2024-01-15 13:21:40 +0900

    v0.7

--- day02.py
+++ day02.py
@@ -5,5 +5,9 @@ print(type(base_number), type(exponent_number))
 #print(f'밑은 {base_number}, 지수는{exponent_number}, 결과 값은 {base_number**exponent_number}')
 print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number ** exponent_number}')
 
-#format function
-print('밑은 {0}, 지수는 {1}, 결과 값은 {2}')
+#format function #밑에 세줄은 최신부터
+print('밑은 {0}, 지수는 {1}, 결과 값은 {2}' .format(base_number, exponent_number, pow(base_number, exponent_number)))
+print('밑은 {}, 지수는 {}, 결과 값은 {}'.format(base_number, exponent_number, pow(base_number)))
+
+#c like
+print('밑은 %d, 지수는 %d, 결과 값은 %d' %(base_number, exponent_number, pow(base_number, exponent_number)))
