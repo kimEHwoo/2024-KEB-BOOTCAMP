6a6f2861173fe711547b55283964a0612c36539b
김태우 <kimtaewoo721@gmail.com>
2024-01-15 14:29:37 +0900

    v1.0

--- day02.py
+++ day02.py
@@ -1,9 +1,2 @@
-dec = 65
-octal = 0o101
-hexadecimal = 0x41
-binary = 0b01000001
-print(dec,octal,hexadecimal,binary)
-print(chr(dec), chr(octal), chr(hexadecimal), chr(binary))
-#chr은 ASCII코드로 보여주는 함수이다. 결과는 A
-print(bin(dec), bin(octal), bin(hexadecimal), bin(binary))
-print(ord('B'), ord('Z'),ord('a'), ord('2')) #66,90,97,50
+fahrenheit = float(input('Input Fahrenheit : '))
+print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0)*5/9):.4f}C')
