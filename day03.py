0ec4c44fde76fdafad47567043d5c58a212e0a90
김태우 <kimtaewoo721@gmail.com>
2024-01-16 14:03:12 +0900

    prime number

--- day03.py
+++ day03.py
@@ -1,12 +1,16 @@
-#preview
+#subjects ={'python':'kim', 'c++' : 'sung', 'data structure':'kim','databsase': 'kang'}
+#print("{0[python]}{0[data structure]}".format(subjects))
 
-subjects = "python c++ database linux"
-print("%e" % 0.7045)
-print(subjects.isalnum())
-subject = input("수강신청과목 입력: ")
+#prime number
+number = int(input("Input number : "))
+cnt =0
+i=1
+while i<=number:
+    if (number % i) == 0:
+        cnt = cnt + 1
+        i = i + 1
 
-try:
-    print(f"해당 과목이 존재합니다. 위치는 {subjects.index('subject')}번 인덱스입니다.")
-except ValueError:
-    print('해당과목이 존재하지 않습니다.')
-
+        if cnt == 2:
+            print(f'{number} is prime number')
+        else:
+            print(f'{number} is not prime number')
