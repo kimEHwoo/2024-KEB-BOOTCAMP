b6d40142771e619fd29a8c0b5a3ef4a7c6eb3654
김태우 <kimtaewoo721@gmail.com>
2024-01-16 11:13:43 +0900

    v1.61

--- day03.py
+++ day03.py
@@ -1,17 +1,13 @@
-# university = r"Inha\nUniversity!" #raw string
-# print(university)
-#
-# university = "Inha\nUniversity!"
-# print(university)
-# number1 = input("First number : ")
-# number2 = input("Second number : ")
-# print(number1 + number2) #concatenation
-# print(number1 * 3) #duplicate
-# print(number1 + 3) #type eroror
+#numbers = input("FirstNumber SecondNumber : ").split() #["9", "7"]
+#print(numbers[0]+numbers[1]) #concatenation
+#print(int(numbers[0]) + int(numbers[1])) #arithetic operation
+#print(9+7)
 
-# slicing
-# print(university[:4])
-# print(university[:-11])
-# print(university[0:len(university)]) #len함수는 글자의 개수 16을 의미한다.
-# print(university[:16])
-# print(university[::2]) #::2는 두 칸씩
+# course = "2024 KEB Bootcamp"
+# print(course)
+# list_course = course.split('B')
+# print(list_course)
+
+subjects = ["python", "c++", "database"]
+subjects_string = "/".join(subjects)
+print(subjects_string)
