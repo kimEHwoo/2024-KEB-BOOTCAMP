ce8c882b87f89bda2d3a78eca88af75531eec9ff
김태우 <kimtaewoo721@gmail.com>
2024-01-16 14:40:39 +0900

    v2.6

--- day03.py
+++ day03.py
@@ -1,19 +1,18 @@
-#subjects ={'python':'kim', 'c++' : 'sung', 'data structure':'kim','databsase': 'kang'}
-#print("{0[python]}{0[data structure]}".format(subjects))
-
 #prime number
 number = int(input("Input number : "))
-is_prime = True #int -> bool
-i = 2
-while i < number:
-    if (number % i) == 0:
-        is_prime = False # remove +
-        break
-        #print(i, end=' ')
-    i = i + 1
+is_prime = True
 
-#if cnt == 0:
-if is_prime:
-            print(f'{number} is prime number')
-else:
-            print(f'{number} is not prime number')
+if number < 2 :
+    print(f'{number} is NOT a prime number')
+else :
+    i = 2
+    while i < number:
+        if (number % i) == 0:
+            is_prime = False
+            break
+        i = i + 1
+
+    if is_prime:
+        print(f'{number} is prime number')
+    else:
+        print(f'{number} is not prime number')
