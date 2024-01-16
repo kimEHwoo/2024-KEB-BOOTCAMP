be31433ca854e2435628fe6165d1aed5acba0396
김태우 <kimtaewoo721@gmail.com>
2024-01-16 15:02:07 +0900

    v2.8 prime number day03

--- day03.py
+++ day03.py
@@ -5,14 +5,12 @@ is_prime = True
 if number < 2 :
     print(f'{number} is NOT a prime number')
 else :
-    i = 2
-    while i < number:
+    for i in range(2, number) :
         if (number % i) == 0:
             is_prime = False
             break
-        i = i + 1
 
     if is_prime:
         print(f'{number} is prime number')
     else:
-        print(f'{number} is not prime number')
+        print(f'{number} is NOT prime number')
