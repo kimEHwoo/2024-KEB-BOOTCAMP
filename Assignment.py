637c91ca72288590a66fbe06f89d9d58a5deaff9
김태우 <kimtaewoo721@gmail.com>
2024-01-17 09:21:31 +0900

    v3.2 assignment day04

--- Assignment.py
+++ Assignment.py
@@ -1,6 +1,56 @@
-#list element
+#Assignment (add prime series program)
 
-my_list = [3,2,1,0]
+while True:
+    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3)prime1	4)prime2	5) Quit program : ")
 
-for value in my_list:
-    print(value)
+    if menu == '1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
+
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+
+    elif menu == '3':
+        number = int(input("Input number : "))
+        is_prime = True
+        if number < 2:
+            print(f'{number} is NOT prime number!')
+        else:
+            i = 2
+            while i < number:
+                if number % i == 0:
+                    is_prime = False
+                    break
+                i = i + 1
+
+            if is_prime:
+                print(f'{number} is prime number')
+            else:
+                print(f'{number} is NOT prime number!')
+
+    elif menu == '4':
+        numbers = (input("Input first second number : ")).split()
+
+        n1 = int(numbers[0])
+        n2 = int(numbers[1])
+
+        for number in range(n1, n2 + 1):
+            is_prime = True
+
+            if number < 2:
+                pass
+            else:
+                for i in range(2, number):
+                    if (number % i) == 0:
+                        is_prime = False
+                        break
+                if is_prime: print(number, end=' ')
+        print()
+
+    elif menu == '5':
+        print('Program terminated')
+        break
+
+    else:
+        print('Invalid menu')
--- Practice.py
+++ Practice.py
@@ -1,4 +1,21 @@
-count = 1
-while count <= 5:
-		print(count)
-		count += 1
+#Assignment (add prime series program)
+
+while True:
+    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3)prime1	4)prime2	5) Quit program : ")
+
+    if menu == '1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+	elif menu == '3':
+		pass
+	elif menu == '4':
+		pass
+
+    elif menu == '5':
+        print('Terminate Program.')
+        break
+	else:
+		print('Invalid Menu')
