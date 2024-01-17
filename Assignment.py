2485091cdd208514c6f2d1bf3ee55cf67b02c73d
김태우 <kimtaewoo721@gmail.com>
2024-01-17 10:09:25 +0900

    v3.31

--- Assignment.py
+++ Assignment.py
@@ -1,24 +1,24 @@
-numbers = (input("Input first second number : ")).split()
+# prime number
+numbers = input("Input first second number : ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
 
-        n1 = int(numbers[0])
-        n2 = int(numbers[1])
+if n1 > n2:
+    n1, n2 = n2, n1
 
+for number in range(n1, n2+1):
+    is_prime = True
 
-
-        for number in range(n1, n2 + 1):
-            is_prime = True
-
-            if number < 2:
-                pass
-            else:
-                i = 2
-                while i * i <= number:   # performance issue
-                    if number % i ==0:
-                        is_prime = False
-                        break
-                    i=i+1
-                    #print(i, end=' ') # loop count
-                if is_prime:
-                    pass
-                    #print(number, end=' ')
-        print()
+    if number < 2:
+        continue  #pass
+    else:
+        i = 2
+        while i*i <= number:  # performance issue
+            if number % i == 0:
+                is_prime = False
+                break
+            i = i + 1
+            #print(i, end=' ')  # loop count
+        if is_prime:
+            pass
+            print(number, end=' ')
