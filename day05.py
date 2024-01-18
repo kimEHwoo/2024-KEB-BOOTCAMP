bfb2ecaf7be4b3e9ca334ba7bdba81196c1b35fc
김태우 <kimtaewoo721@gmail.com>
2024-01-18 09:24:49 +0900

    v4.3 assignment day05

---
+++ day05.py
@@ -0,0 +1,9 @@
+# univ = 'inha university'
+# counts_alphabet = {letter : univ.count(letter) for letter in univ}
+# print(counts_alphabet)
+
+univ = 'inha university'
+counts_alphabet = dict()
+for letter in univ:
+    counts_alphabet[letter] = univ.count(letter)
+print(counts_alphabet)
