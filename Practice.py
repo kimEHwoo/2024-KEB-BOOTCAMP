cd60388fd6a76b0c472c7bc5a3942a35314832ef
김태우 <kimtaewoo721@gmail.com>
2024-01-22 11:05:43 +0900

    v6.9 day07

---
+++ Practice.py
@@ -0,0 +1,12 @@
+class Bill():
+    def __init__(self, description):
+        self.description = description
+class Tail():
+    def __init__(self, length):
+        self.length = length
+class Duck():
+    def __init__(self, bill, tail):
+        self.bill = bill
+        self.tail = tail
+    def about(self):
+        print('This duck has a', self.bill.description, 'bill and a', self.tail.length, 'tail')
